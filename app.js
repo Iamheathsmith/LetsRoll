41107fbfd4f3a26e09c383685d4cc753861ef0be
Jeffery Sloan <jefferysloanjr@gmail.com>
2017-11-06 10:29:21 -0800

    button and function created who goes first

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 'use strict';
+var whoGoesFirst = function () {
+  var goFirst = ['Who just had a Birtday?', 'Who is the youngest?', 'Who is the oldest'];
+  var randomFirst = goFirst[Math.floor(Math.random() * goFirst.length)];
+  var goFirstBtn = document.getElementById('goFirstBtn');
+  return onClick(randomFirst);
+};
 
-var goFirst = ['Who just had a Birtday?', 'Who is the youngest?', 'Who is the oldest'];
-
-var randomFirst = goFirst[Math.floor(Math.random() * goFirst.length)];
+whoGoesFirst ();
