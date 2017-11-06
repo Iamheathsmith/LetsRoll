4ca23e05cff9296a866642865e7561527b74c5e7
Jeffery Sloan <jefferysloanjr@gmail.com>
2017-11-06 10:50:42 -0800

    alert created for who goes first button

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var whoGoesFirst = function () {
   var goFirst = ['Who just had a Birtday?', 'Who is the youngest?', 'Who is the oldest'];
   var randomFirst = goFirst[Math.floor(Math.random() * goFirst.length)];
   var goFirstBtn = document.getElementById('goFirstBtn');
-  return onClick(randomFirst);
+  goFirstBtn (alert(randomFirst));
 };
 
 whoGoesFirst ();
